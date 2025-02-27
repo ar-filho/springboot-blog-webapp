30a042a62b7cf93284440ea785d66c66767f2a2d
ar-filho <airton.r.filho@ufv.br>
2025-02-27 16:15:26 -0300

    Spring Initializr

---
+++ src/test/java/com/arfilho/springboot_blog_webapp/SpringbootBlogWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arfilho.springboot_blog_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBlogWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
