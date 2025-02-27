e8c5d5204f2b6da334835f6b7b6f38d6f597ff9e
ar-filho <airton.r.filho@ufv.br>
2025-02-27 16:52:37 -0300

    Configure MySQL Database in Spring Boot App

--- src/main/java/com/arfilho/springboot_blog_webapp/SpringbootBlogWebappApplication.java
+++ src/main/java/com/arfilho/springboot_blog_webapp/SpringbootBlogWebappApplication.java
@@ -2,22 +2,11 @@ package com.arfilho.springboot_blog_webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-//@SpringBootApplication
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class SpringbootBlogWebappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBlogWebappApplication.class, args);
-
-		Student student = new Student();
-		student.setId(1);
-		student.setName("John Doe");
-		student.setEmail("john.doe@gmail.com");
-
-		System.out.println(student.getId());
-		System.out.println(student.getName());
-		System.out.println(student.getEmail());
 	}
 }
