cbf3ce7a8e47d45e0d58b9de8d51600e7037b9ac
ar-filho <airton.r.filho@ufv.br>
2025-02-27 16:29:59 -0300

    Using Lombok (Resolved Lombok Related Issues)

--- src/main/java/com/arfilho/springboot_blog_webapp/SpringbootBlogWebappApplication.java
+++ src/main/java/com/arfilho/springboot_blog_webapp/SpringbootBlogWebappApplication.java
@@ -2,12 +2,22 @@ package com.arfilho.springboot_blog_webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SpringbootBlogWebappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBlogWebappApplication.class, args);
-	}
 
+		Student student = new Student();
+		student.setId(1);
+		student.setName("John Doe");
+		student.setEmail("john.doe@gmail.com");
+
+		System.out.println(student.getId());
+		System.out.println(student.getName());
+		System.out.println(student.getEmail());
+	}
 }
---
+++ src/main/java/com/arfilho/springboot_blog_webapp/Student.java
@@ -0,0 +1,13 @@
+package com.arfilho.springboot_blog_webapp;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Student {
+
+    private int id;
+    private String name;
+    private String email;
+}
