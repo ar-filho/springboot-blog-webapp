a1f94edee77c3843cc9a9c8082b51572402b2aea
ar-filho <airton.r.filho@ufv.br>
2025-02-28 15:44:55 -0300

    Create PostMapper

---
+++ src/main/java/com/arfilho/springboot_blog_webapp/mapper/PostMapper.java
@@ -0,0 +1,34 @@
+package com.arfilho.springboot_blog_webapp.mapper;
+
+import com.arfilho.springboot_blog_webapp.dto.PostDto;
+import com.arfilho.springboot_blog_webapp.entity.Post;
+
+public class PostMapper {
+
+    // map Post entity to PostDto
+    public static PostDto mapToPostDto(Post post) {
+        return PostDto.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .url(post.getUrl())
+                .content(post.getContent())
+                .shortDescription(post.getShortDescription())
+                .createdOn(post.getCreatedOn())
+                .updatedOn(post.getUpdatedOn())
+                .build();
+    }
+
+    // map PostDto to Post entity
+    public static Post mapToPost(PostDto postDto) {
+        return Post.builder()
+                .id(postDto.getId())
+                .title(postDto.getTitle())
+                .url(postDto.getUrl())
+                .content(postDto.getContent())
+                .shortDescription(postDto.getShortDescription())
+                .createdOn(postDto.getCreatedOn())
+                .updatedOn(postDto.getUpdatedOn())
+                .build();
+    }
+
+}
