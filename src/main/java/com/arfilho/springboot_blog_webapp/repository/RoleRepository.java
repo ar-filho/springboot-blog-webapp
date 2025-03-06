c4842f96187f424fd5c38a21ff3b19bc018fd104
ar-filho <airton.r.filho@ufv.br>
2025-03-06 14:16:01 -0300

    Revert "Registration Feature - UserRepository and RoleRepository"
    This reverts commit 7459afb3a691da6c039a5ba351ca09793f8aa0fa.

--- src/main/java/com/arfilho/springboot_blog_webapp/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.arfilho.springboot_blog_webapp.repository;
-
-import com.arfilho.springboot_blog_webapp.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
