9abe420f1dd6396695a332cce4ec58460cce2717
ar-filho <airton.r.filho@ufv.br>
2025-03-06 14:30:14 -0300

    Registration Feature - Create Handler Method to Handle Registration Form Request

---
+++ src/main/java/com/arfilho/springboot_blog_webapp/dto/RegistrationDto.java
@@ -0,0 +1,18 @@
+package com.arfilho.springboot_blog_webapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistrationDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
