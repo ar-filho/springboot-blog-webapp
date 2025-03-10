667f3e152b01c77fe171d3cd7ac4d7b3cf524b72
ar-filho <airton.r.filho@ufv.br>
2025-03-10 10:12:26 -0300

    feat: configure spring security for client side
    Doing #1

--- src/main/java/com/arfilho/springboot_blog_webapp/config/WebSpringSecurity.java
+++ src/main/java/com/arfilho/springboot_blog_webapp/config/WebSpringSecurity.java
@@ -38,6 +38,9 @@ public class WebSpringSecurity {
                                                 authorize.requestMatchers(new AntPathRequestMatcher("/resources/**")).permitAll()
                                                         .requestMatchers(new AntPathRequestMatcher("/register/**")).permitAll()
                                                         .requestMatchers(new AntPathRequestMatcher("/admin/**")).hasAnyRole("ADMIN", "GUEST")
+                                                        .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
+                                                        .requestMatchers(new AntPathRequestMatcher("/post/**")).permitAll()
+                                                        .anyRequest().authenticated()
                                 )
                                 .formLogin(form -> form
                                         .loginPage(("/login"))
