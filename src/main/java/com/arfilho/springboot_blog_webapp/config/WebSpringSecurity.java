c86c6332d555176c08ec3964b3b393f2d0d45c11
ar-filho <airton.r.filho@ufv.br>
2025-03-10 08:59:41 -0300

    feat: configure spring security for roles
    Doing #1

--- src/main/java/com/arfilho/springboot_blog_webapp/config/WebSpringSecurity.java
+++ src/main/java/com/arfilho/springboot_blog_webapp/config/WebSpringSecurity.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -17,7 +18,10 @@ public class WebSpringSecurity {
                     try {
                         csrf.disable()
                                 .authorizeHttpRequests(
-                                        (authorize) -> authorize.anyRequest().authenticated()
+                                        (authorize) ->
+                                                authorize.requestMatchers(new AntPathRequestMatcher("/resources/**")).permitAll()
+                                                        .requestMatchers(new AntPathRequestMatcher("/register/**")).permitAll()
+                                                        .requestMatchers(new AntPathRequestMatcher("/admin/**")).hasAnyRole("ADMIN", "GUEST")
                                 )
                                 .formLogin(form -> form
                                         .loginPage(("/login"))
